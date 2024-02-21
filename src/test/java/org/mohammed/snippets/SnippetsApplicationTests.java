8e8a0380944f079ef3f4ba4b6e46b617218a4f7e
mohammedelmor <mohammedelmor7@gmail.com>
2024-02-21 11:03:20 +0400

    Initial Commit

---
+++ src/test/java/org/mohammed/snippets/SnippetsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mohammed.snippets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnippetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
